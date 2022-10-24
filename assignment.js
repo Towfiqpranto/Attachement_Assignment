ce8ac79eb5e9c41ca4dff36abb662318dbb86efe
Towfiqpranto <90025316+Towfiqpranto@users.noreply.github.com>
2022-10-25 02:02:00 +0600

    Delete assignment.js

--- assignment.js
+++
@@ -1,7 +0,0 @@
-let input=document.getElementById('input')
-let button=document.getElementById('button')
-let current=document.getElementById('output').innerHTML
-button.addEventListener("click",myfunc)
-function myfunc(){
-    document.getElementById('output').innerHTML=current+' '+input.value
-}
